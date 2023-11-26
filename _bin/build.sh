e55c96af846b657f38d709e650fba5fb2877ceda
Finne Boonen <hennar@gmail.com>
2023-11-26 14:12:46 +0100

    fix archery marshals

--- _bin/build.sh
+++ _bin/build.sh
@@ -3,7 +3,6 @@
 # Omitted:
 # "https://scripts.drachenwald.sca.org/json/groups-special.json"
 declare -a ScriptsDocuments=(
-  "archery-marshals"
   "archery-progression"
   "archery-ranks"
   "fullcalendar"
@@ -15,7 +14,7 @@ done
 
 declare -a DisDocuments=(
   "academyofdefense"
-  "archery-marshal"
+  "archery-marshals"
   "branches"
   "chartered_peerage"
   "courtreports"
