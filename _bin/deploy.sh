85a81d91fae810fbfa390976c7488a0d0c451ac8
Finne Boonen <hennar@gmail.com>
2024-09-14 18:30:04 +0200

    Update deploy.sh

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -1,32 +1,3 @@
 #!/bin/bash
 
-eval $(ssh-agent -s)
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
-chmod 644 ~/.ssh/known_hosts
-echo "${OVH_PRIVATE}" | tr -d '\r' | ssh-add - > /dev/null
-
-if [ -n $GITHUB_REF_NAME ]
-then
-  if [ "$GITHUB_REF_NAME" == "main" ]
-  then
-    echo "Building main site"
-    JEKYLL_ENV=production bundle exec jekyll build -d public
-    rsync -rvzc --exclude '.htaccess' --delete -e 'ssh -p 45333' public/ $WEBHOST_OVH:drach-main/public/
-    rsync -rvzc --delete -e 'ssh -p 45333' public/.htaccess $WEBHOST_OVH:drach-main/
-  elif [ "$GITHUB_REF_NAME" == "staging" ]
-  then
-    echo "Building staging site"
-    JEKYLL_ENV=staging bundle exec jekyll build -d public
-    rsync -rvzc --exclude '.htaccess' --delete -e 'ssh -p 45333' public/ $WEBHOST_OVH:drach-staging/public/
-    rsync -rvzc --delete -e 'ssh -p 45333' public/.htaccess $WEBHOST_OVH:drach-staging/
-  else
-    echo "Taking over staging site with ${GITHUB_REF_NAME}"
-    JEKYLL_ENV=development bundle exec jekyll build -d public
-    rsync -rvzc --exclude '.htaccess' --delete -e 'ssh -p 45333' public/ $WEBHOST_OVH:drach-staging/public/
-    rsync -rvzc --delete -e 'ssh -p 45333' public/.htaccess $WEBHOST_OVH:drach-staging/
-  fi
-else
-  echo "No branch specified"
-fi
+echo "don't upload, not dw branch"
